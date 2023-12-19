6a35d1fdffaeecee34a55619f615e1bf1c9cf535
Justin Chiu <justinchiu@g.harvard.edu>
2023-12-19 16:08:07 -0500

    footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    // https://ileumas.com/writing/2022/03/astro-math-katex/
+    markdown: {
+        remarkPlugins: [
+            remarkMath,
+        ],
+        rehypePlugins: [
+            rehypeKatex,
+        ]
+    },
 	site: 'https://justinchiu.netlify.app',
 	integrations: [mdx(), sitemap()],
 });
