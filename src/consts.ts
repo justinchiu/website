abbd733ac4523c99df1a89959912decfdab74f4e
Justin Chiu <justinchiu@g.harvard.edu>
2023-12-19 16:28:21 -0500

    change name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Justin Chiu\'s Website';
-export const SITE_DESCRIPTION = 'Justin Chiu\'s Website';
+export const SITE_TITLE = 'Justin T Chiu\'s Website';
+export const SITE_DESCRIPTION = 'Justin T Chiu\'s Website';
